f36691543297228a93ad91b250360941ca5cd5d7
fujitaken <kentyan199@yahoo.co.jp>
2021-04-11 20:38:01 +0900

    make it the same as v_realtime

--- rupan_pj/src/components/HelloWorld.vue
+++ rupan_pj/src/components/HelloWorld.vue
@@ -1,58 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div>
+		<p class="hello">{{word}}!</p>
+		<div>
+			<input type="text"
+			v-bind:disabled="isDis"
+			@keydown.enter="isEnter"
+			@compositionstart="composing=true"
+			@compositionend="composing=false"
+			v-on:input="write" 
+			v-model="word"
+			>
+		</div>
+	</div>
 </template>
 
 <script>
+    var keyPush = new Audio('');
+    var enterPush = new Audio('');
+
+    keyPush.src = "../typewriter.mp3";
+    keyPush.load();
+
+    enterPush.src = "../rupanIcatch.wav";
+    enterPush.load();
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data:function(){
+	return {
+		word:'',
+		composing:false,
+		isDis:false,
+	};
+  },
+  methods:{
+	write:function(event) {
+		if(event.inputType == undefined || event.inputType === "deleteContentBackward"){
+			return;
+		} else {
+			keyPush.currentTime = 0;
+			keyPush.play();
+		}
+		this.word = event.target.value;
+	},
+	isEnter:async function() {
+		if(!this.composing){
+			this.isDis = true;
+			this.word = '';
+			enterPush.currentTime = 0.5;
+			enterPush.play();
+			const res = await this.donotWrite(enterPush);
+			console.log(res);
+			this.isDis = false;
+		}
+	},
+	donotWrite:function(aud) {
+		return new Promise((resolve) => {
+			aud.addEventListener('ended',()=>{
+				resolve('done');
+			});
+		});
+	}
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hello {
+	font-size:20pt;
+	font-weight:bold;
+	border:1px solid magenta;
+	padding:5px 10px 0px 10px;
+	margin:10px;
 }
 </style>
